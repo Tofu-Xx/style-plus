3389162819e433b583935dc9c050ee613d2e3fa0
Tofu-Xx <1714843907@qq.com>
2024-10-13 04:48:16 +0800

    chore: dom content loaded

--- main.js
+++ main.js
@@ -7,28 +7,21 @@ const getSelector = (el) => {
       break;
     }
     const siblings = [...el.parentNode.children];
-    const count = siblings.filter(sibling => sibling.tagName === tag).length;
-    const nth = count > 1 ? `:nth-child(${1 + siblings.indexOf(el)})` : '';
+    const nth = `:nth-child(${1 + siblings.indexOf(el)})` 
     path.unshift(tag + nth);
   };
   return `${path.join(' > ')}`.toLowerCase();
 };
-const handleStyle = (el) => {
-  let style = el.getAttribute('style');
-  el.removeAttribute('style');
-  return `${getSelector(el)}{ ${style}}`;
-};
-new MutationObserver((mutations) => {
-  for (const mutation of mutations) { 
-    const added = mutation.addedNodes[0]
-    if (added.nodeType !== 1) continue;
-    // @ts-ignore
-    if (!added?.hasAttribute('style')) continue;
-    // @ts-ignore
-    if (!added?.getAttribute('style').trim().startsWith('&')) continue;
-    const style = handleStyle(added);
-    const styleEl = document.createElement('style');
-    styleEl.innerHTML = style
-    document.head.appendChild(styleEl);
-  }
-}).observe(document, { childList: true, subtree: true });
+document.addEventListener('DOMContentLoaded', () => {
+  const els = document.querySelectorAll('[style*="&"][style*="{"][style*="}"]');
+  let css = '';
+  els.forEach(el => {
+    const style = el.getAttribute('style');
+    if (!style?.trim().startsWith('&')) return;
+    const selector = getSelector(el);
+     css +=  style.replace('&', selector);
+  });
+  const styleEl = document.createElement('style');
+  styleEl.innerHTML = css;
+  document.head.appendChild(styleEl);
+})
