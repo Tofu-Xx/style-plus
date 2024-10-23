b3ccf94562436da7ba2f771cd6d7d5f20babf4b4
Tofu-Xx <1714843907@qq.com>
2024-10-23 16:41:59 +0800

    perf: CSSStyleSheet iife

--- main.js
+++ main.js
@@ -1,48 +1,51 @@
-function getSelector(el) {
-  const path = []
-  while (el && el.nodeType === Node.ELEMENT_NODE) {
-    let selector = el.tagName.toLowerCase()
-    if (el.id) {
-      selector = `#${el.id}`
+;(() => {
+  function getSelector(el) {
+    const path = []
+    while (el && el.nodeType === Node.ELEMENT_NODE) {
+      let selector = el.tagName.toLowerCase()
+      if (el.id) {
+        selector = `#${el.id}`
+        path.unshift(selector)
+        break
+      }
+      if (el.className) {
+        selector += `.${el.className.trim().replace(/\s+/g, '.')}`
+      }
+      const siblings = el.parentNode ? [...el.parentNode.children].filter(child => child.tagName === el.tagName) : []
+      if (siblings.length > 1) {
+        const index = siblings.indexOf(el) + 1
+        selector += `:nth-of-type(${index})`
+      }
       path.unshift(selector)
-      break
+      el = el.parentNode
     }
-    if (el.className) {
-      selector += `.${el.className.trim().replace(/\s+/g, '.')}`
-    }
-    const siblings = el.parentNode ? [...el.parentNode.children].filter(child => child.tagName === el.tagName) : []
-    if (siblings.length > 1) {
-      const index = siblings.indexOf(el) + 1
-      selector += `:nth-of-type(${index})`
-    }
-    path.unshift(selector)
-    el = el.parentNode
+    return path.join(' > ')
   }
-  return path.join(' > ')
-}
 
-const styleEl = document.createElement('style')
-document.head.appendChild(styleEl)
-const csss = new Set()
-new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    for (const node of mutation.addedNodes) {
-      if (node.nodeType !== Node.ELEMENT_NODE)
-        continue
-      const style = node?.getAttribute('style')
-      if (!style?.trim().startsWith('&'))
-        continue
-      node.removeAttribute('style')
-      const css = style.replace('&', getSelector(node))
-      if (csss.has(css))
-        continue
-      csss.add(css)
-      styleEl.innerHTML += css
+  const csss = new Set()
+  const sheet = new CSSStyleSheet()
+  document.adoptedStyleSheets.push(sheet)
+
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (node.nodeType !== Node.ELEMENT_NODE)
+          continue
+        const style = node?.getAttribute('style')
+        if (!style?.trim().startsWith('&'))
+          continue
+        node.removeAttribute('style')
+        const css = style.replace('&', getSelector(node))
+        if (csss.has(css))
+          continue
+        csss.add(css)
+        sheet.insertRule(css)
+      }
     }
-  }
-}).observe(document.documentElement, {
-  subtree: true,
-  attributes: true,
-  attributeFilter: ['style'],
-  childList: true,
-})
+  }).observe(document.documentElement, {
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['style'],
+    childList: true,
+  })
+})()
