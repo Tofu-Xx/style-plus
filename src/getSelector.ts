6596bbd9129c9b449e62b82ed1bb8147d100e2ba
Tofu-Xx <1714843907@qq.com>
2026-03-21 02:51:07 +0800

    chore: update

--- src/getSelector.ts
+++ src/getSelector.ts
@@ -1,3 +1,12 @@
+/**
+ * 为指定的 DOM元素 生成唯一路径的 CSS 选择器。
+ * 该选择器基于元素的标签名及其在同级元素中按类型的位置（:nth-of-type），
+ * 从目标元素一直向上追溯到根元素（不包括 document 或非元素节点）。
+ *
+ * @param {Element} el - 需要生成选择器的 DOM 元素
+ * @returns {string} 生成的 CSS 选择器字符串
+ *
+ */
 export function getSelector(el: Element) {
   let path = ''
   while (el?.nodeType === Node.ELEMENT_NODE) {
